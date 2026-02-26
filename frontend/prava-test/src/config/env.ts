93ec9ba02e9e4942caa042825cb20f21b9c0e44c
JavohirBozorboyev <javohirbozorboyev2002@gmail.com>
2026-02-26 10:41:01 +0500

    url torilash

--- frontend/prava-test/src/config/env.ts
+++ frontend/prava-test/src/config/env.ts
@@ -3,8 +3,7 @@ export const ENV = {
   GOOGLE_CLIENT_ID:
     import.meta.env.VITE_GOOGLE_CLIENT_ID ||
     "237372892439-4bju17u6k3cjoil26p148m21ilmecd9s.apps.googleusercontent.com",
-  API_BASE_URL:
-    import.meta.env.VITE_API_BASE_URL || "http://164.68.100.190:8080",
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "",
   TELEGRAM_BOT_ID: Number(import.meta.env.VITE_TELEGRAM_BOT_ID) || 8485868847,
   TELEGRAM_BOT_USERNAME:
     import.meta.env.VITE_TELEGRAM_BOT_USERNAME || "pravaonlineuzbot",
